461850a98a4cb1e72ff7ec9d098fa99fb2e2a170
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2021-02-09 21:07:52 +0100

    feat(index): Ajoute les ancres et effets smooth JS onScroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,8 @@ function removeClass(index) {
   var element = document.getElementById(`item-hover-${index}`);
   element.classList.remove("nav-hover");
 }
+
+function onScroll(){
+  var el = document.getElementById('scrollTop');
+  window.scrollY  > 100 ? el.style.display = "block" : el.style.display = "none";
+}
