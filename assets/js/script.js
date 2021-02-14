7156a7838e4683a72b6a910a05ab8b3648f88fca
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2021-02-15 00:03:21 +0100

    Refactor complet

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+/** Hover item navigation */
+function addClass(index) {
+  var element = document.getElementById(`item-hover-${index}`);
+  element.classList.add("nav-hover");
+}
+
+function removeClass(index) {
+  var element = document.getElementById(`item-hover-${index}`);
+  element.classList.remove("nav-hover");
+}
+
+/** Show button scrollTop */
+function onScroll(){
+  var el = document.getElementById('scrollTop');
+  window.scrollY  > 100 ? el.style.display = "block" : el.style.display = "none";
+}
